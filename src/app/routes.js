a81b645bf57bfac430e622e6526361b257a9fcea
fahadalzubaidi <s202271360@kfupm.edu.sa>
2026-04-10 07:50:55 +0300

    feat: configure client routes and initialize task categories/mock data

---
+++ src/app/routes.js
@@ -0,0 +1,87 @@
+import { createBrowserRouter } from "react-router";
+import { Landing } from "./pages/Landing";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import { BrowseServices } from "./pages/BrowseServices";
+import { ClientDashboard } from "./pages/ClientDashboard";
+import { PostTask } from "./pages/PostTask";
+import { RequestDetails } from "./pages/RequestDetails";
+import { CompareOffers } from "./pages/CompareOffers";
+import { JobWorkspace } from "./pages/JobWorkspace";
+import { ProviderDashboard } from "./pages/ProviderDashboard";
+import { CreateService } from "./pages/CreateService";
+import { BrowseTasks } from "./pages/BrowseTasks";
+import { ProviderProfile } from "./pages/ProviderProfile";
+import { AdminDashboard } from "./pages/AdminDashboard";
+import { Messages } from "./pages/Messages";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Landing,
+  },
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/signup",
+    Component: Signup,
+  },
+  {
+    path: "/services",
+    Component: BrowseServices,
+  },
+  {
+    path: "/services/:id",
+    Component: BrowseServices, // In a real app, this would be ServiceDetail page
+  },
+  {
+    path: "/client/dashboard",
+    Component: ClientDashboard,
+  },
+  {
+    path: "/client/post-task",
+    Component: PostTask,
+  },
+  {
+    path: "/client/request/:taskId",
+    Component: RequestDetails,
+  },
+  {
+    path: "/client/compare-offers/:taskId",
+    Component: CompareOffers,
+  },
+  {
+    path: "/client/tasks/:taskId/offers",
+    Component: CompareOffers,
+  },
+  {
+    path: "/client/jobs/:jobId",
+    Component: JobWorkspace,
+  },
+  {
+    path: "/provider/dashboard",
+    Component: ProviderDashboard,
+  },
+  {
+    path: "/provider/create-service",
+    Component: CreateService,
+  },
+  {
+    path: "/provider/tasks",
+    Component: BrowseTasks,
+  },
+  {
+    path: "/providers/:providerId",
+    Component: ProviderProfile,
+  },
+  {
+    path: "/admin",
+    Component: AdminDashboard,
+  },
+  {
+    path: "/messages",
+    Component: Messages,
+  },
+]);
